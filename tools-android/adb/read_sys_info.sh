1477a2aee03aaf016f5706d90f24546f36447ca9
luHub <lugu16f@gmail.com>
2024-12-13 16:48:05 +0100

    updata install and read

--- tools-android/adb/read_sys_info.sh
+++ tools-android/adb/read_sys_info.sh
@@ -13,8 +13,5 @@ echo "***************** System Info ********************************************
 adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell getprop > system.info
 echo "***************** Features Info *********************************************"
 adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell pm list features > features.info
-echo "***************** Subsystems Info *********************************************"
-adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell dumpsys media.camera > camera.info
-adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell dumpsys media.camera > camera.info
 
 
