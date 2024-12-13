fcf1d5e7bbdd27098d660869a9d52e4fddd10fca
luHub <lugu16f@gmail.com>
2024-12-13 16:47:20 +0100

    updata install and read

--- tools-android/adb/read_sys_info.sh
+++ tools-android/adb/read_sys_info.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "***************** Extracting System information wih adb *****************"
+adb version
+echo
+echo "*************************************************************************"
+echo " Before Next Step, connect your Android device and turn on Developer Mode"
+echo
+echo 
+echo "***************** Extracting *********************************************"
+echo "***************** System Info ********************************************"
+
+adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell getprop > system.info
+echo "***************** Features Info *********************************************"
+adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell pm list features > features.info
+echo "***************** Subsystems Info *********************************************"
+adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell dumpsys media.camera > camera.info
+adb devices | tail -n2 | cut -f1  | xargs | xargs -I {} adb -s {}  shell dumpsys media.camera > camera.info
+
+
