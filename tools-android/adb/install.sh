21871ecf46d0cfba690ab41c7f72dcac1367349e
luHub <lugu16f@gmail.com>
2025-01-06 15:24:06 +0100

    rename directory

--- tools-android/adb/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing Android Debug Bridge(adb)"
-sudo apt install adb
-echo "Finishing installing adb"
-adb version
--- tools-android/source/source.sh
+++
@@ -1,4 +0,0 @@
-echo "https://source.android.com/"
-echo "downloading Android 4.1.2_r1"
-repo init -u https://android.googlesource.com/platform/manifest -b android-4.1.2_r1
-repo sync
