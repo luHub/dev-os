0ea42e17bd28b89eb460d913345120e096eba471
luHub <lugu16f@gmail.com>
2025-01-07 21:17:34 +0100

    add setup

--- kernel-tools/kernel/setup.sh
+++ kernel-tools/kernel/setup.sh
@@ -3,6 +3,18 @@
 vxy='v6.x'
 xyz='6.12.8'
 
+# Download and install tools
+sudo apt-get install make
+sudo apt-get install imagemagick graphviz dvipng python3-venv gcc latexmk librsvg2-bin texlive-lang-chinese texlive-xetex
+sudo apt-get install python3-sphinx
+
+# Download and install Kernel
 mkdir src
 wget -P src  www.kernel.org/pub/linux/kernel/$vxy/linux-$xyz.tar.gz
 cd src && tar xvzf linux-$xyz.tar.gz 
+
+# Generate Documentation
+cd linux-$xyz && make pdfdocs
+
+
+echo "Please read the README file on top directory as a starting point. Exiting"
