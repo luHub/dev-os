d4c6d1ae42612c8e70ee44dfdbabb8791f6e0ccf
luHub <lucioguerchi16f@gmail.com>
2026-03-16 09:03:00 +0100

    Update wey.sh

--- kernel-tools/community/wey.sh
+++ kernel-tools/community/wey.sh
@@ -8,4 +8,8 @@
 # Body: Explain what you changed and why
 # Sign-off: if is a checkpatch warning, explain  which #one.
 
-# (3)  
+# (3) Check Grammar spell 
+
+# (4) Check Subject
+# (5) Check Body
+# (6) Check Sign-off
