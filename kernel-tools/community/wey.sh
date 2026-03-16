d231ed416d56d2686e63c4d6b8c871967ab00a7a
luHub <lucioguerchi16f@gmail.com>
2026-03-16 09:01:15 +0100

    Update wey.sh

--- kernel-tools/community/wey.sh
+++ kernel-tools/community/wey.sh
@@ -1,4 +1,11 @@
 #/!bin/bash
 
 
-# Do only one change per patch
+# (1) Do only one change per patch.
+
+# (2) Check Commit:  
+# Subject: staging: <driver_name>: <summary>
+# Body: Explain what you changed and why
+# Sign-off: if is a checkpatch warning, explain  which #one.
+
+# (3)  
